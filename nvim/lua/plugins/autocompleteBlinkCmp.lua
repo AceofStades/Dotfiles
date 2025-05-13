a4d710142bb0eef29ff74929765b49b41c0dce0f
AceofStades <rahulpbokade@gmail.com>
2025-05-14 02:08:04 +0530

    I don't know what I'm doing

---
+++ nvim/lua/plugins/autocompleteBlinkCmp.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "saghen/blink.cmp",
+        opts = {
+            keymap = {
+                preset = "enter",
+                ["<Tab>"] = { "select_next", "fallback" },
+                ["<S-Tab>"] = { "select_prev", "fallback" },
+            },
+            completion = {
+                list = {
+                    selection = "manual",
+                },
+            },
+        },
+    },
+}
