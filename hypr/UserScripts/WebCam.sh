720d8890aaa4da5e24b95e150d243b9fd487b8c9
AceofStades <rahulpbokade@gmail.com>
2025-06-01 20:53:46 +0530

    Minor changes

--- hypr/UserScripts/WebCam.sh
+++ hypr/UserScripts/WebCam.sh
@@ -5,9 +5,9 @@ WebCamIcon="$iDIR/webcam.png"
 WebCamOff="$iDIR/webcam-off.png"
 
 if lsmod | grep -q uvcvideo; then
-    notify-send -e -h string:x-canonical-private-synchronous:volume_notif -u low -i "$WebCamOff" "Disabling WebCam..."
+    notify-send -e -h string:x-canonical-private-synchronous:volume_notif -u low -i "$WebCamOff" "Enter pass to Disable WebCam..."
     pkexec modprobe -r uvcvideo
 else
-    notify-send -e -h string:x-canonical-private-synchronous:volume_notif -u low -i "$WebCamIcon" "Enabling WebCam..."
+    notify-send -e -h string:x-canonical-private-synchronous:volume_notif -u low -i "$WebCamIcon" "Enter pass to Enable WebCam..."
     pkexec modprobe uvcvideo
 fi
